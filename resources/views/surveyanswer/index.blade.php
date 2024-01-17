ef918bc83b760086d5ab55a019fb1a261b545d8c
上園俊介 <s.uezono@cae-sar.co.jp>
2024-01-17 21:16:55 +0900

    一覧画面のデザインを調整した

--- resources/views/surveyanswer/index.blade.php
+++ resources/views/surveyanswer/index.blade.php
@@ -9,33 +9,33 @@
       <table>
         <thead class="bg-blue-100">
           <tr>
-            <th>回答日</th>
-            <th>質問1</th>
-            <th>質問2</th>
-            <th>質問3</th>
-            <th>質問4</th>
-            <th>質問5</th>
-            <th>質問6</th>
-            <th>質問7</th>
-            <th>質問8</th>
-            <th>質問9</th>
-            <th>質問10</th>
+            <th class="whitespace-nowrap">回答日</th>
+            <th class="whitespace-nowrap">質問1</th>
+            <th class="whitespace-nowrap">質問2</th>
+            <th class="whitespace-nowrap">質問3</th>
+            <th class="whitespace-nowrap">質問4</th>
+            <th class="whitespace-nowrap">質問5</th>
+            <th class="whitespace-nowrap">質問6</th>
+            <th class="whitespace-nowrap">質問7</th>
+            <th class="whitespace-nowrap">質問8</th>
+            <th class="whitespace-nowrap">質問9</th>
+            <th class="whitespace-nowrap">質問10</th>
           </tr>
         </thead>
         <tbody class="bg-white">
           @foreach ($surveyanswers as $surveyanswer)
           <tr>
             <td class="answered_at">{{ $surveyanswer->answered_at }}</td>
-            <td>{{ $surveyanswer->answer_text_01 }}</td>
-            <td>{{ $surveyanswer->answer_text_02 }}</td>
-            <td>{{ $surveyanswer->answer_text_03 }}</td>
-            <td>{{ $surveyanswer->answer_text_04 }}</td>
-            <td>{{ $surveyanswer->answer_text_05 }}</td>
-            <td>{{ $surveyanswer->answer_text_06 }}</td>
-            <td>{{ $surveyanswer->answer_text_07 }}</td>
-            <td>{{ $surveyanswer->answer_text_08 }}</td>
-            <td>{{ $surveyanswer->answer_text_09 }}</td>
-            <td>{{ $surveyanswer->answer_text_10 }}</td>
+            <td class="whitespace-nowrap">{{ $surveyanswer->answer_text_01 }}</td>
+            <td class="min-w-80">{{ $surveyanswer->answer_text_02 }}</td>
+            <td class="min-w-80">{{ $surveyanswer->answer_text_03 }}</td>
+            <td class="min-w-80">{{ $surveyanswer->answer_text_04 }}</td>
+            <td class="min-w-80">{{ $surveyanswer->answer_text_05 }}</td>
+            <td class="min-w-80">{{ $surveyanswer->answer_text_06 }}</td>
+            <td class="min-w-80">{{ $surveyanswer->answer_text_07 }}</td>
+            <td class="whitespace-nowrap">{{ $surveyanswer->answer_text_08 }}</td>
+            <td class="whitespace-nowrap">{{ $surveyanswer->answer_text_09 }}</td>
+            <td class="whitespace-nowrap">{{ $surveyanswer->answer_text_10 }}</td>
           </tr>
           @endforeach
         </tbody>
@@ -48,10 +48,9 @@ th, td {
   border: solid .5px #d3d3d3;
   padding: .5rem;
   text-align: center;
-  min-width: 100px;
   max-width: 300px;
 }
-.answered_at {
+th, .answered_at {
   white-space: nowrap;
 }
 </style>
