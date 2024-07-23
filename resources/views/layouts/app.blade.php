7d6d06621a9a21a81477af904f20884bba34ab9f
上園俊介 <s.uezono@cae-sar.co.jp>
2024-07-23 15:48:01 +0900

    レスポンシブ対応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
-                    <div class="mx-24 py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="sm:mx-24 py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
