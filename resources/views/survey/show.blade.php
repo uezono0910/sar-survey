73bff0da98819453c76088d3b5658e11f50da7e2
上園俊介 <s.uezono@cae-sar.co.jp>
2024-09-13 13:44:10 +0900

    アンケート詳細

--- resources/views/survey/show.blade.php
+++ resources/views/survey/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
   <x-slot name="header">
       <h2 class="font-semibold text-xl text-gray-800 leading-tight overflow-x-scroll">
-        {{ $surveyItems->date }}のアンケート
+        {{ $surveys->title }}の回答（{{ $surveys->date }}）
       </h2>
   </x-slot>
   <div class="mx-12 py-12">
@@ -9,22 +9,24 @@
       <table class="w-full">
         <thead class="bg-blue-100">
           <tr>
+          <th class="whitespace-nowrap p-2 text-left">投稿日</th>
           @foreach($surveyItems as $surveyItem)
             <th class="min-w-64 p-2 text-left">{{ $surveyItem->content }}</th>
           @endforeach
           </tr>
         </thead>
         <tbody class="bg-white">
-          @foreach ($surveyanswers as $surveyanswer)
+          @foreach ($surveyAnswers as $surveyAnswer)
             <tr>
-              @foreach($surveyItem as $surveyItem)
-                <td>
-                @foreach ($surveyanswerdetails as $surveyanswerdetail)
-                  @if ($surveyanswer->id === $surveyanswerdetail->survey_answer_id && $survey->id === $surveyanswerdetail->survey_id)
-                    {{ $surveyanswerdetail->answer }}
+              <td class="whitespace-wrap p-2 text-left">{{ $surveyAnswer->created_at }}</td>
+              @foreach($surveyItems as $surveyItem)
+              <td>
+                @foreach ($surveyAnswerDetails as $surveyAnswerDetail)
+                  @if ($surveyAnswer->id == $surveyAnswerDetail->survey_answer_id && $surveyItem->id == $surveyAnswerDetail->survey_item_id)
+                    {{ $surveyAnswerDetail->answer }}
                   @endif
                 @endforeach
-                </td>
+              </td>
               @endforeach
             </tr>
           @endforeach
@@ -33,8 +35,14 @@
     </div>
   </div>
 </x-app-layout>
+
 <style>
 th, td {
   border: solid .5px #d3d3d3;
+  padding: .5rem;
+  max-width: 300px;
+}
+th {
+  white-space: wrap;
 }
 </style>
